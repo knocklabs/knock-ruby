2486ba57084e3f4822aa4eb5863c22c14f82b1b8
Chris Bell <cjbell1988@gmail.com>
2022-05-01 22:44:22 -0500

    feat(objects): add preferences support

--- lib/knock/objects.rb
+++ lib/knock/objects.rb
@@ -111,11 +111,27 @@ module Knock
       # @param [Hash] channel_data channel data
       #
       # @return [Hash] channel data
-      def set_channel_data(id:, channel_id:, channel_data:)
+      def set_channel_data(collection:, id:, channel_id:, channel_data:)
         request = put_request(
           auth: true,
           path: "/v1/objects/#{collection}/#{id}/channel_data/#{channel_id}",
           body: {data: channel_data}
+        )
+
+        execute_request(request: request)
+      end
+
+      # Unsets object channel data for the given channel id
+      #
+      # @param [String] collection The collection the object is in
+      # @param [String] id The object id
+      # @param [String] channel_id target channel ID
+      #
+      # @return [Hash] channel data
+      def unset_channel_data(collection:, id:, channel_id:)
+        request = delete_request(
+          auth: true,
+          path: "/v1/objects/#{collection}/#{id}/channel_data/#{channel_id}"
         )
 
         execute_request(request: request)
@@ -133,6 +149,145 @@ module Knock
           auth: true,
           path: "/v1/objects/#{collection}/#{id}/messages",
           params: options
+        )
+
+        execute_request(request: request)
+      end
+
+      ##
+      # Preferences
+      ##
+
+      # Returns all preference sets for the user
+      #
+      # @param [String] user_id The ID of the user to retrieve preferences for
+      #
+      # @return [Hash] The preference sets
+      def get_all_preferences(collection:, id:)
+        endpoint = "/v1/objects/#{collection}/#{id}/preferences"
+
+        request = get_request(
+          auth: true,
+          path: endpoint
+        )
+
+        execute_request(request: request)
+      end
+
+      # Gets a single preference set, defaults to the 'default' set
+      # for the user given.
+      #
+      # @param [String] collection The collection the object is in
+      # @param [String] id The object id
+      # @param [String] preference_set The preference set ID (defaults to `default`)
+      #
+      # @return [Hash] The preference set (if it exists)
+      def get_preferences(collection:, id:, preference_set: DEFAULT_PREFERENCE_SET_ID)
+        endpoint = "/v1/objects/#{collection}/#{id}/preferences/#{preference_set}"
+
+        request = get_request(
+          auth: true,
+          path: endpoint
+        )
+
+        execute_request(request: request)
+      end
+
+      # Sets multiple preferences at once for the preference set.
+      #
+      # @param [String] collection The collection the object is in
+      # @param [String] id The object id
+      # @param [String] preference_set The preference set ID (defaults to `default`)
+      # @param [Hash] channel_types The channel_types hash to set
+      # @param [Hash] workflows The workflows hash to set
+      # @param [Hash] categories The categories hash to set
+      #
+      # @return [Hash] The preference set
+      def set_preferences(
+        collection:,
+        id:,
+        channel_types: nil,
+        workflows: nil,
+        categories: nil,
+        preference_set: DEFAULT_PREFERENCE_SET_ID
+      )
+        endpoint = "/v1/objects/#{collection}/#{id}/preferences/#{preference_set}"
+
+        request = put_request(
+          auth: true,
+          path: endpoint,
+          body: {
+            channel_types: channel_types,
+            workflows: workflows,
+            categories: categories
+          }
+        )
+
+        execute_request(request: request)
+      end
+
+      # Sets preferences for the given channel type
+      #
+      # @param [String] collection The collection the object is in
+      # @param [String] id The object id
+      # @param [String] preference_set The preference set ID (defaults to `default`)
+      # @param [String] channel_type The channel type to set
+      # @param [Bool] setting Whether the channel type is enabled or not
+      #
+      # @return [Hash] The preference set
+      def set_channel_type_preferences(collection:, id:, channel_type:, setting:, preference_set: DEFAULT_PREFERENCE_SET_ID)
+        endpoint = "/v1/objects/#{collection}/#{id}/preferences/#{preference_set}/channel_types/#{channel_type}"
+
+        request = put_request(
+          auth: true,
+          path: endpoint,
+          body: {subscribed: setting}
+        )
+
+        execute_request(request: request)
+      end
+
+      # Sets preferences for the given workflow
+      #
+      # @param [String] collection The collection the object is in
+      # @param [String] id The object id
+      # @param [String] preference_set The preference set ID (defaults to `default`)
+      # @param [String] workflow The workflow to set preferences for
+      # @param [Bool | Hash] setting Either a boolean to indicate if the type is enabled
+      #  or a hash containing channel types and settings
+      #
+      # @return [Hash] The preference set
+      def set_workflow_preferences(collection:, id:, workflow:, setting:, preference_set: DEFAULT_PREFERENCE_SET_ID)
+        params = setting.is_a?(Hash) ? setting : {subscribed: setting}
+        endpoint = "/v1/objects/#{collection}/#{id}/preferences/#{preference_set}/workflows/#{workflow}"
+
+        request = put_request(
+          auth: true,
+          path: endpoint,
+          body: params
+        )
+
+        execute_request(request: request)
+      end
+
+      # Sets preferences for the given category
+      #
+      # @param [String] collection The collection the object is in
+      # @param [String] id The object id
+      # @param [String] preference_set The preference set ID (defaults to `default`)
+      # @param [String] category The category to set preferences for
+      # @param [Bool | Hash] setting Either a boolean to indicate if the type is enabled
+      #  or a hash containing channel types and settings
+      #
+      # @return [Hash] The preference set
+      def set_category_preferences(collection:, id:, category:, setting:, preference_set: DEFAULT_PREFERENCE_SET_ID)
+        params = setting.is_a?(Hash) ? setting : {subscribed: setting}
+        endpoint = "/v1/objects/#{collection}/#{id}/preferences/#{preference_set}/categories/#{category}"
+
+        request = put_request(
+          auth: true,
+          path: endpoint,
+          body: params
         )
 
         execute_request(request: request)
