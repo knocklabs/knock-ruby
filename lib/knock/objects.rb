3a815a6df316cc081063f1fb02974eb978a4dc94
Chris Bell <cjbell1988@gmail.com>
2023-03-27 17:38:14 -0400

    feat(object): introduce object subs

--- lib/knock/objects.rb
+++ lib/knock/objects.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require 'json'
-require 'net/http'
-require 'uri'
+require "json"
+require "net/http"
+require "uri"
 
 module Knock
   # Methods for interacting with objects in Knock
@@ -12,7 +12,7 @@ module Knock
       include Base
       include Client
 
-      DEFAULT_PREFERENCE_SET_ID = 'default'
+      DEFAULT_PREFERENCE_SET_ID = "default"
 
       # Retrieves an Object in a collection
       #
@@ -56,7 +56,7 @@ module Knock
         request = post_request(
           auth: true,
           path: "/v1/objects/#{collection}/bulk/set",
-          body: { objects: objects }
+          body: {objects: objects}
         )
 
         execute_request(request: request)
@@ -87,7 +87,7 @@ module Knock
         request = post_request(
           auth: true,
           path: "/v1/objects/#{collection}/bulk/delete",
-          body: { object_ids: object_ids }
+          body: {object_ids: object_ids}
         )
 
         execute_request(request: request)
@@ -121,7 +121,7 @@ module Knock
         request = put_request(
           auth: true,
           path: "/v1/objects/#{collection}/#{id}/channel_data/#{channel_id}",
-          body: { data: channel_data }
+          body: {data: channel_data}
         )
 
         execute_request(request: request)
@@ -246,16 +246,16 @@ module Knock
       #
       # @return [Hash] The preference set
       def set_channel_type_preferences(collection:,
-                                       id:,
-                                       channel_type:,
-                                       setting:,
-                                       preference_set: DEFAULT_PREFERENCE_SET_ID)
+        id:,
+        channel_type:,
+        setting:,
+        preference_set: DEFAULT_PREFERENCE_SET_ID)
         endpoint = "/v1/objects/#{collection}/#{id}/preferences/#{preference_set}/channel_types/#{channel_type}"
 
         request = put_request(
           auth: true,
           path: endpoint,
-          body: { subscribed: setting }
+          body: {subscribed: setting}
         )
 
         execute_request(request: request)
@@ -272,7 +272,7 @@ module Knock
       #
       # @return [Hash] The preference set
       def set_workflow_preferences(collection:, id:, workflow:, setting:, preference_set: DEFAULT_PREFERENCE_SET_ID)
-        params = setting.is_a?(Hash) ? setting : { subscribed: setting }
+        params = setting.is_a?(Hash) ? setting : {subscribed: setting}
         endpoint = "/v1/objects/#{collection}/#{id}/preferences/#{preference_set}/workflows/#{workflow}"
 
         request = put_request(
@@ -295,13 +295,65 @@ module Knock
       #
       # @return [Hash] The preference set
       def set_category_preferences(collection:, id:, category:, setting:, preference_set: DEFAULT_PREFERENCE_SET_ID)
-        params = setting.is_a?(Hash) ? setting : { subscribed: setting }
+        params = setting.is_a?(Hash) ? setting : {subscribed: setting}
         endpoint = "/v1/objects/#{collection}/#{id}/preferences/#{preference_set}/categories/#{category}"
 
         request = put_request(
           auth: true,
           path: endpoint,
           body: params
+        )
+
+        execute_request(request: request)
+      end
+
+      # Lists all subscriptiopns for the given object
+      #
+      # @param [String] collection The collection the object is in
+      # @param [String] id The object id
+      # @param [Hash] options Options to pass to the subscriptions endpoint
+      #
+      # @return [Hash] Paginated subscriptions response
+      def list_subscriptions(collection:, id:, options: {})
+        request = get_request(
+          auth: true,
+          path: "/v1/objects/#{collection}/#{id}/subscriptions",
+          params: options
+        )
+
+        execute_request(request: request)
+      end
+
+      # Creates a set of subscriptions for the recipients on the object
+      #
+      # @param [String] collection The collection the object is in
+      # @param [String] id The object id
+      # @param [Array<String, Hash>] recipients An array of recipients
+      # @param [Hash] properties A set of custom properties to set on the subscriptions
+      #
+      # @return [Array<Hash>] List of created subscriptions
+      def add_subscriptions(collection:, id:, recipients: [], properties: {})
+        request = post_request(
+          auth: true,
+          path: "/v1/objects/#{collection}/#{id}/subscriptions",
+          body: {recipients: recipients, properties: properties}
+        )
+
+        execute_request(request: request)
+      end
+
+      # Removes subscriptions for the recipients on the object
+      #
+      # @param [String] collection The collection the object is in
+      # @param [String] id The object id
+      # @param [Array<String, Hash>] recipients An array of recipients
+      #
+      # @return [Array<Hash>] List of deleted subscriptions
+      def delete_subscriptions(collection:, id:, recipients: [])
+        request = delete_request(
+          auth: true,
+          path: "/v1/objects/#{collection}/#{id}/subscriptions",
+          body: {recipients: recipients}
         )
 
         execute_request(request: request)
