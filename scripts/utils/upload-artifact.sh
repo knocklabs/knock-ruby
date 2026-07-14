33460f2b59debc42bc7048ed2845fff588f815ab
Thomas <thomas.whyyou@gmail.com>
2026-07-14 12:18:25 -0400

    Build SDK
    Stainless-Generated-From: 8aee6253735f63a547c08ed6edb0f9ffd60bc471

--- scripts/utils/upload-artifact.sh
+++ scripts/utils/upload-artifact.sh
@@ -110,4 +110,4 @@ walk_tree "$DIST_DIR"
 echo "::endgroup::"
 
 echo -e "${GREEN}Gem artifacts uploaded to Stainless storage.${NC}"
-echo -e "\033[32mInstallation: bundle remove knockapi && bundle add knockapi --source 'https://pkg.stainless.com/s/knock-ruby/$SHA'\033[0m"
+echo -e "\033[32mInstallation: bundle remove knockapi && bundle add knockapi --source 'https://pkg.stainless.com/s/knock-ruby-staging/$SHA'\033[0m"
